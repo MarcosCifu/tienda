6c738b71d411f5bee07550ca593346b7fb3486dc
vagrant <vagrant@homestead>
2018-06-07 21:39:34 +0000

    first commit

---
+++ resources/assets/js/components/products/ProductsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <material-transition-group
+                tag="div"
+                name="fadeIn"
+                class="row">
+            <products-card-component :key="product.id" :data-index="index" v-bind:product="product" v-for="(product, index) in products" ></products-card-component>
+        </material-transition-group>
+    </section>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                name: 'Products Component',
+                products: [],
+                endpoint: "/productos"
+            }
+        },
+        created (){
+            this.fetchProducts();
+        },
+        methods: {
+            fetchProducts(){
+                axios.get(this.endpoint).then((response) => {
+                   this.products = response.data.data;
+                });
+            }
+        }
+    }
+</script>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app');
+@section('content')
+    <div class="container">
+        <div>
+            <products-component></products-component>
+        </div>
+        <div class="row">
+
+        </div>
+        <div class="actions text-center">
+            {{$products->links()}}
+        </div>
+    </div>
+@endsection
